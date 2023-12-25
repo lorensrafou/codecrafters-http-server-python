0675f29900f32da2692c2f6bc2bceca4b2d03e6b
lorensrafou <lrafou@gmail.com>
2023-12-24 21:36:01 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
- Uncomment this to pass the first stage
+@@ -1,16 +1,10 @@
 
+import socket
+def main():
 def main():
     
     print("Logs from your program will appear here!")
-  
-    
+
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept() # wait for client
 if __name__ == "__main__":
     main()
