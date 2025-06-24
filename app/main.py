6d63de8c6d9c804fdf8846468ac3b1ce7b03d5da
lorensrafou <lrafou@gmail.com>
2025-06-24 15:53:15 -0700

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept()[0].send("HTTP/1.1 200 OK\r\n\r\n".encode())
     server_socket.accept() # wait for client
 
 
